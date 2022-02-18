3dcfa4457f2bd64f99bc95f085c4574346ea3ca0
martiserra99 <martiserramolina99@gmail.com>
2022-02-18 09:27:37 +0100

    Setup canvas-ui.js main structure

--- js/canvas-ui/canvas-ui.js
+++ js/canvas-ui/canvas-ui.js
@@ -1,21 +1,37 @@
 export const canvasUI = {
   ui: {
-    new(selector) {},
+    new(selector) {
+      return new UI(selector);
+    },
   },
 
   view: {
     _types: new Map(),
 
-    type(name) {},
+    type(name) {
+      const type = new ViewType(name);
+      this._types.set(name, type);
+      return type;
+    },
 
-    new(type) {},
+    new(type) {
+      const type = this._types.get(type);
+      return new View(id, type);
+    },
   },
 
   layout: {
     _types: new Map(),
 
-    type(name) {},
+    type(name) {
+      const type = new LayoutType(name);
+      this._types.set(name, type);
+      return type;
+    },
 
-    new(type) {},
+    new(type) {
+      const type = this._types.get(type);
+      return new Layout(id, type);
+    },
   },
 };
