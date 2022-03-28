63355298f10f1e320086ec306ef2cd0b54e9598b
Marti Serra <martiserramolina99@gmail.com>
2022-03-28 17:52:23 +0200

    Add area view

--- js/script.js
+++ js/script.js
@@ -2,42 +2,25 @@ import { canvasUI } from "./canvas-ui/canvas-ui.js";
 
 const ui = canvasUI.ui.new("#ui");
 
-const root = canvasUI.layout.new("root", "grid");
+const root = canvasUI.layout.new("root", "frame");
 
-root.set("size", {
-  width: "auto",
-  height: "auto",
-});
-root.set("dimensions", {
-  columns: [{ count: 2, unit: "px", value: 300 }],
-  rows: [{ count: 2, unit: "px", value: 300 }],
-});
-root.set("gap", {
-  size: { horizontal: 20, vertical: 50 },
-  color: "#000",
-});
-root.set("alignContent", { horizontal: "right", vertical: "bottom" });
-root.set("alignItems", {
-  horizontal: "right",
+const area = canvasUI.view.new("area", "area");
+
+area.set("background", "#767");
+
+area.set("border", { size: 5, color: "#000" });
+
+area.set("corner", { type: "round", size: 10 });
+
+root.insert(area);
+
+area.layoutParams.set("align", {
+  horizontal: "middle",
   vertical: "middle",
 });
-root.set("border", {
-  size: 5,
-  color: "#000",
-});
-root.set("corner", {
-  type: "cut",
-  size: 50,
-});
 
-const textArea1 = canvasUI.composite.new("textArea-1", "textArea");
-const textArea2 = canvasUI.composite.new("textArea-2", "textArea");
-
-root.insert(textArea1);
-root.insert(textArea2);
-textArea1.layoutParams.set("position", {
-  row: 1,
-  column: 1,
+area.listeners.add("click", function (area) {
+  area.set("background", "#a768");
 });
 
 ui.start(root);
