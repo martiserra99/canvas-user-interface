b26cc38e286e23c796710a38e38a332626a91d22
Marti Serra <martiserramolina99@gmail.com>
2024-01-18 18:57:07 +0100

    Solved mistake type data in signal

--- src/types.ts
+++ src/types.ts
@@ -5,7 +5,7 @@ import { State } from './element/generic/dependencies/events';
 export type Size = { width: number; height: number };
 export type Coords = { x: number; y: number };
 
-export type Signal = { type: string; data: Coords | string };
+export type Signal = { type: string; data: any };
 
 export type Check = (
   element: Element,
